78a92894157a2d118f388cd1e5b356e9c9c3c498
Greg Laptop <77gregory07@gmail.com>
2020-11-02 19:06:26 -0600

    Created Product Table for Receiving Desk page

---
+++ frontend/src/components/receivingdesk/ProductTable.js
@@ -0,0 +1,56 @@
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableContainer from '@material-ui/core/TableContainer';
+import TableHead from '@material-ui/core/TableHead';
+import TableRow from '@material-ui/core/TableRow';
+import Paper from '@material-ui/core/Paper';
+
+const useStyles = makeStyles({
+  table: {
+    minWidth: 650,
+  },
+});
+
+function createData(part_number, description, qty) {
+  return { part_number, description, qty};
+}
+
+const rows = [
+  createData(1,'windsheild w/ polymer', 10),
+  createData(2,'car1', 10),
+  createData(3,'car2', 10),
+  createData(4,'car3', 10),
+  createData(5,'car4', 10),
+];
+
+export default function ProductTable() {
+  const classes = useStyles();
+
+  return (
+    <TableContainer component={Paper}>
+      <Table className={classes.table} aria-label="simple table">
+        <TableHead>
+          <TableRow>
+            <TableCell>Part Number</TableCell>
+            <TableCell align="right">Description</TableCell>
+            <TableCell align="right">Quantity</TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {rows.map((row) => (
+            <TableRow key={row.part_number}>
+              <TableCell component="th" scope="row">
+                {row.part_number}
+              </TableCell>
+              <TableCell align="right">{row.description}</TableCell>
+              <TableCell align="right">{row.qty}</TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </TableContainer>
+  );
+}
--- frontend/src/pages/ReceivingDeskPage.js
+++ frontend/src/pages/ReceivingDeskPage.js
@@ -1,13 +1,14 @@
 import '../css/App.css';
 import {BrowserRouter } from 'react-router-dom';
 import Navbar from '../components/core/employeeNav.js';
+import ProductTable from '../components/receivingdesk/ProductTable';
 
 function ReceivingDeskPage() {
   return (
     <div className="App">
     <Navbar />
       Hello ReceivingDeskPage!
-
+      <ProductTable/>
     </div>
   );
 }
