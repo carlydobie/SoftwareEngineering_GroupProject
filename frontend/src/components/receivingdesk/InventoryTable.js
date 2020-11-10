fc5e12825a508cdf071142d0f2ccffdca22de13c
Greg Laptop <77gregory07@gmail.com>
2020-11-09 20:04:45 -0600

    Icons

--- frontend/src/components/receivingdesk/InventoryTable.js
+++ frontend/src/components/receivingdesk/InventoryTable.js
@@ -56,7 +56,9 @@ export default function ProductTable(props) {
          //change a state to true
       },
   };
-  
+  function setData([]) {
+    console.log("dsfads")
+  }
 
 
     let data = Array.from(entries);
@@ -75,11 +77,60 @@ export default function ProductTable(props) {
                   }
                 }}
             /> */}
+            <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons"></link>
+
+
             <MaterialTable
               title={"Inventory"}
               data={data}
               columns={column}
-            />
+              editable={{
+                isEditable: rowData => rowData.name === 'a', // only name(a) rows would be editable
+                isEditHidden: rowData => rowData.name === 'x',
+                isDeletable: rowData => rowData.name === 'b', // only name(b) rows would be deletable,
+                isDeleteHidden: rowData => rowData.name === 'y',
+                onBulkUpdate: changes => 
+                    new Promise((resolve, reject) => {
+                        setTimeout(() => {
+                            /* setData([...data, newData]); */
+        
+                            resolve();
+                        }, 1000);
+                    }),
+                onRowAddCancelled: rowData => console.log('Row adding cancelled'),
+                onRowUpdateCancelled: rowData => console.log('Row editing cancelled'),
+                onRowAdd: newData =>
+                    new Promise((resolve, reject) => {
+                        setTimeout(() => {
+                            /* setData([...data, newData]); */
+        
+                            resolve();
+                        }, 1000);
+                    }),
+                onRowUpdate: (newData, oldData) =>
+                    new Promise((resolve, reject) => {
+                        setTimeout(() => {
+                            const dataUpdate = [...data];
+                            const index = oldData.tableData.id;
+                            dataUpdate[index] = newData;
+                            setData([...dataUpdate]);
+        
+                            resolve();
+                        }, 1000);
+                    }),
+                onRowDelete: oldData =>
+                    new Promise((resolve, reject) => {
+                        setTimeout(() => {
+                            const dataDelete = [...data];
+                            const index = oldData.tableData.id;
+                            dataDelete.splice(index, 1);
+                            setData([...dataDelete]);
+        
+                            resolve();
+                        }, 1000);
+                    })
+            }}
+        />
         </div>
     );
 }
